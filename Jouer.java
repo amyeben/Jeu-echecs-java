83c3e53ece3534daf5e70e7c5a34cd0393898757
KaenatSYED <78016870+KaenatSYED@users.noreply.github.com>
2021-06-01 13:32:16 +0200

    Create Jouer.java

---
+++ Jouer.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Jouer
+{
+    public static void main(String[] args)
+    {
+   /*___________Saisie générale*/
+        Scanner saisie = new Scanner(System.in);
+
+  
+    }
+
+
+}
