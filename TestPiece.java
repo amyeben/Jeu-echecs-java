e8903804d4a6ec684d23d411bfecb74cdc2bfde9
Amy Eben <77935411+amyeben@users.noreply.github.com>
2021-06-02 20:32:58 +0200

    Add files via upload

---
+++ TestPiece.java
@@ -0,0 +1,15 @@
+
+public class TestPiece {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Piece piece1 = new Piece(1,1,1);
+		Piece piece2 = new Piece(2,2,0);
+		
+		System.out.println(piece1.toString());
+		System.out.println(piece2.toString());
+		
+		System.out.println("Test ok !");
+	}
+
+}
