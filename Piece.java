00100b7fc5fc136867adc395ca3730b947347710
KaenatSYED <78016870+KaenatSYED@users.noreply.github.com>
2021-06-02 20:22:22 +0200

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -1,122 +1,71 @@
-public abstract class Piece
-{
-  private int couleur;	// 0 pour noir, 1 pour blanc
-  private int ligne;	// position x
-  private int colonne;	// position y
+public class Piece {
+	
+//	Declaration des instances
+	
+	private int couleur; 
+	private int ligne;
+	private int colonne;
 
-    // Constructeur avec paramètres
-  public Piece(){
-  }
+// Getters et Setters
 
-  public Piece(int ligne, int colonne, int couleur)
-  {
-    this.ligne = ligne;
-    this.colonne = colonne;
-    this.couleur = couleur;
-  }
-
-    public boolean deplacer(Echiquier e,Case arrive){
-    Case depart = e.getCase(this.ligne, this.colonne);
-    System.out.println(depart.getPosLigne() + " et : " + depart.getPosColonne());
-    System.out.println(arrive.getPosLigne() + " et : " + arrive.getPosColonne());
-    System.out.println(depart.getPiece().coupPossible(e,arrive));
-    if(depart.getPiece().coupPossible(e,arrive)) {
-          System.out.println("entre du if 1");
-          e.setCase(arrive, depart.getPiece());
-          System.out.println(e.getCase(arrive.getPosLigne(),arrive.getPosColonne()));
-          depart.setPiece(null);
-          System.out.println(e.getCase(depart.getPosLigne(),depart.getPosColonne()));
-          return true;
-      }
-    return false;
-  }
-
-   public boolean memeCouleur(Case depart, Case arrive) {
-    if(!(arrive.estVide()) )
-      if(arrive.getPiece().getCouleur()==depart.getPiece().getCouleur())
-        return true;
-    return false;
-  }
+	public int getCouleur() {
+		return couleur;
+	}
 
 
-  public boolean coupPossible(Echiquier e, Case arrive)
-  {
-    return true;
-  }
+	public void setCouleur(int couleur) {
+		this.couleur = couleur;
+	}
 
 
-    /* GETTERS */
-    public int getPosLigne()
-    {
-      return this.ligne;
-    }
-
-  public int getPosColonne()
-    {
-      return this.colonne;
-    }
-
-    public int getCouleur()
-    {
-      return this.couleur;
-    }
-
-    public String getPropriete() {
-      return "";
-    }
-
-  /*___________________FIN DES GETTERS	*/
-
-    /* SETTERS */
-  public void setPosLigneEtColonne(int ligne, int colonne) {
-    this.setPosLigne(ligne);
-    this.setPosColonne(colonne);
-  }
-    public void setPosLigne(int ligne)
-    {
-      this.ligne = ligne;
-    }
-
-    public void setPosColonne(int colonne)
-    {
-      this.colonne = colonne;
-    }
-
-    public void setCouleur(int c)
-    {
-      this.couleur = c;
-    }
-  /*FIN DES SETTERS	*/
-
-    /*// Deplacer piece a redefinir dans les objects tour, cavalier etc...
-     public void deplacerPiece(Echiquier e, Case arrive)
-     {
-       System.out.println("test 1");
-       if (this.verifDeplacement(e,arrive))
-       {
-          int x = this.getPosLigne();
-          int y = this.getPosColonne();
-          int i = arrive.getPosLigne();
-          int j = arrive.getPosColonne();
-          System.out.println("Ligne " + x + " Colonne " + y);
-          System.out.println("i " + i + " j " + j);
-          e.setCase(e.getCase(i,j),e.getCase(x,y).getPiece());
-          System.out.println(e.getCase(i,j));
-          e.setCase(e.getCase(x,y),null);
-          System.out.println(e.getCase(x,y));
-       }
-     }*/
+	public int getLigne() {
+		return ligne;
+	}
 
 
-  /* Methode toString */
-    public String toString()
-    {
-      String s ="";
-      if(this.couleur == 0)
-        s = "noir";
-      else
-        s = "blanc";
+	public void setLigne(int ligne) {
+		this.ligne = ligne;
+	}
 
-        return "Piece [" + this.ligne+";"  + this.colonne+ "]" + "couleur : " + s;
-    }
+
+	public int getColonne() {
+		return colonne;
+	}
+
+
+	public void setColonne(int colonne) {
+		this.colonne = colonne;
+	}
+	
+// Constructeur vide 
+	
+	public Piece(){
+	  }
+
+// Constructeur Champs à Champs
+	
+	public Piece(int ligne, int colonne, int couleur)
+	  {
+	    this.ligne = ligne;
+	    this.colonne = colonne;
+	    this.couleur = couleur;
+	  }
+	
+// Redéfinition de la méthode toString() de la classe Object
+	
+	public String couleurpiece() {
+		String s;
+		if(this.couleur == 0)
+	        s = "blanc";
+	      else
+	        s = "noir";
+		return s;
+		
+	}
+	
+	public String toString() {
+		
+		return ("La pièce se situe à la ligne"+getLigne()+", et à la colonne"+ this.colonne+". Elle est de couleur "+couleurpiece()+".");
+		 
+	}}
 }
