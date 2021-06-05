2b54d71ecbb233517cb6370969a52f2c9d815c1e
Amy Eben <77935411+amyeben@users.noreply.github.com>
2021-06-06 01:12:07 +0200

    Update Reine.java

--- Reine.java
+++ Reine.java
@@ -1,4 +1,3 @@
-
 public class Reine extends Piece{
 
 	// Déclaration d'instances
@@ -48,5 +47,142 @@ public class Reine extends Piece{
 	{
 		return super.toString() + " et la piece ressemble à ça: " + this.propriete;
 	}
+	
+	public boolean  verifDeplacement(Echiquier e,Case arrive) {
+		Case depart = e.getCase(this.getLigne(), this.getColonne());
+		
+		if(depart.memeColonne(arrive)) 
+			if(this.boucleColonne(e, depart, arrive))
+				return true;
+				
+		if(depart.memeLigne(arrive)) 
+			if(this.boucleLigne(e, depart, arrive))
+				return true;
+		
+		if(this.boucleDiagonale(e, depart, arrive))
+			return true;
+		
+		return false;
+				
+	}
+	
+	
+	public boolean boucleDiagonale(Echiquier e,Case depart, Case arrive)
+	{
+				int i = arrive.getLigne();
+			int j = arrive.getColonne();
+			if(depart.getLigne()>i && depart.getColonne()<j)  //en haut a droite
+			{
+				int x = depart.getLigne() - 1;
+				int y = depart.getColonne() + 1;
+				
+				if(!(e.getCase(x, y).CaseVide())) 
+					return false;
+				
+				while(x > i && y < j)
+				{
+					if (!(e.estVide(e.getCase(x,y)) ))
+						return false;
+					x = x - 1;
+					y = y + 1;
+				}
+			}
+			else if(depart.getLigne()>i && depart.getColonne()>j) {//en haut a gauceh
+				int x = depart.getLigne() - 1;
+				int y = depart.getLigne() - 1;
+				if(!(e.getCase(x, y).CaseVide())) 
+					return false;
+				
+				while(x > i && y > j)
+				{
+					
+					if (!(e.estVide(e.getCase(x,y)) ))
+						return false;
+					x = x - 1;
+					y = y - 1;
+				}
+				
+			}
+			else if (depart.getLigne()<i && depart.getColonne()>j) {//en bas a gauche
+				int x = depart.getLigne() + 1;
+				int y = depart.getColonne() - 1;	
+				if(!(e.getCase(x, y).CaseVide())) 
+					return false;
+				while(x < i && y > j)
+				{
+					if (!(e.estVide(e.getCase(x,y)) ))
+						return false;
+					x = x + 1;
+					y = y - 1;
+				}
+				
+			}
+			else if (depart.getLigne()<i && depart.getColonne()<j) {//en bas a droite
+				int x = depart.getLigne() + 1;
+				int y = depart.getColonne() + 1;
+				
+				if(!(e.getCase(x, y).CaseVide())) 
+					return false;
+				while(x < i && y < j)
+				{
+					if (!(e.estVide(e.getCase(x,y)) ))
+						return false;
+					x = x + 1;
+					y = y + 1;
+				}
+				
+			}
+			
+			if(super.memeCouleur(depart, arrive))
+
+				return false;
+		
+		return true;
+	}
+	
+	public boolean boucleColonne(Echiquier e, Case depart, Case arrive) {
+		if(arrive.getLigne()>depart.getLigne())
+		{
+			for(int i=depart.getLigne()+1; i<arrive.getLigne();i++) 
+			{
+				System.out.println("Case, i"+i+e.getCase(i, depart.getColonne()));
+				if(!(e.getCase(i, depart.getColonne()).CaseVide()))
+					return false;
+			}
+			
+		}else 
+			for(int i=arrive.getLigne()+1; i<depart.getLigne();i++) 
+			{
+				System.out.println("Case, i"+i+e.getCase(i, depart.getColonne()));
+				if(!(e.getCase(i, depart.getLigne()).CaseVide()))
+					return false;
+			}
+		
+		if(super.memeCouleur(depart, arrive))
+			return false;
+		
+		return true;
+	}
+	
+	public boolean boucleLigne(Echiquier e, Case depart, Case arrive) {
+		if(arrive.getColonne()>depart.getColonne())
+		{
+			for(int j=depart.getColonne()+1; j<arrive.getColonne();j++) {
+				System.out.println("Case, j"+j+e.getCase(depart.getLigne(),j));
+				if(!(e.getCase(depart.getLigne(),j).CaseVide()))
+					return false;
+			}
+		}else
+			for(int j=arrive.getColonne()+1; j<depart.getColonne();j++) {
+				System.out.println("Case, j"+j+e.getCase(depart.getLigne(),j));
+				if(!(e.getCase(depart.getLigne(),j).CaseVide()))
+					return false;
+			}
+		if(super.memeCouleur(depart, arrive))
+				return false;
+		
+		return true;
+	}
+	
 
 }
